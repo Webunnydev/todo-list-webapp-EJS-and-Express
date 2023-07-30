b923cd5b2e9e0e60c4db53ba9b73d01f588110e1
Yamuna Raja <webbyperso@gmail.com>
2023-07-30 22:53:36 +0530

    todo list

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
  
+var listItem=[];
 
 app.get("/", (req, res) => {
     var today = new Date().toLocaleDateString('en-us', { day:"numeric", month:"long", weekday:"long" });
-    res.render("index.ejs", {Today: today});
+    res.render("index.ejs", {Today: today, item: listItem});
+});
+
+app.post("/", (req, res) => {
+    var newItem = req.body.listItem;
+    listItem.push(newItem);
+    res.redirect("/");
 });
  
 
