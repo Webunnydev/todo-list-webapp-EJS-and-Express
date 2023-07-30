50e51c30c4bc074db1242eff9f64aabab2d15a07
Yamuna Raja <webbyperso@gmail.com>
2023-07-30 06:52:56 +0530

    Heading

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import express from "express";
 
 const app = express();
+const port = 3000;
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen (port, () => {
+    console.log("Listens successfully at port:", port);
+});
