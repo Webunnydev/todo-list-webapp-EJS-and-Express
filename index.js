49c87119a152d28792ca5732f3316da176adf316
Yamuna Raja <webbyperso@gmail.com>
2023-07-30 08:24:03 +0530

    input field

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+ 
 
 app.get("/", (req, res) => {
     var today = new Date().toLocaleDateString('en-us', { weekday:"long", month:"long", day:"numeric"});
